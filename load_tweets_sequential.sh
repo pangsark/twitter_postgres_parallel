1c44d784d407020d9ae1d010fe4c3c225e272c79
Sangeon Park <sangpark@g.hmc.edu>
2024-04-14 23:02:16 -0700

    verifying again for correctness

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -8,7 +8,7 @@ echo '==========================================================================
 
 time for file in $files; do
     echo
-    time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1351/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1351/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
@@ -16,12 +16,13 @@ echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
     echo
-    time python3 load_tweets.py --db=postgresql://postgres:pass@localhost:13511 --inputs "$file"
+    python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13511/ --inputs $file
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:135111/ --inputs $file
+time python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13512/ --inputs $file
 done
+
