e13f5f0248ffb6fbbfc77fee4aafa7024bf3b76f
Sangeon Park <sangpark@g.hmc.edu>
2024-04-14 22:26:43 -0700

    verifying correctness

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,11 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1352/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1353/ --inputs
