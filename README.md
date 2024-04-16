fd47004f129cabf0bfe47005a0182f2393b49683
Sangeon Park <sangpark@g.hmc.edu>
2024-04-15 21:32:47 -0700

    checked everything

--- README.md
+++ README.md
@@ -280,9 +280,9 @@ Ensure that your runtimes on the lambda server are recorded below.
 
 |                        | elapsed time (sequential) | elapsed time (parallel)   |
 | -----------------------| ------------------------- | ------------------------- |
-| `pg_normalized`        |                           |                           | 
-| `pg_normalized_batch`  |                           |                           | 
-| `pg_denormalized`      |                           |                           | 
+| `pg_normalized`        | 10.76sec                  | 4.09sec                   | 
+| `pg_normalized_batch`  | 6min 9.12sec              | 58.78sec                  | 
+| `pg_denormalized`      | 1min 58.92sec             | 15.23sec                  | 
 
 Then upload a link to your forked github repo on sakai.
 
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,14 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
+time parallel ./load_denormalized.sh ::: $files
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13511/ --inputs
+time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13511/ --inputs={} ::: $files
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13512/ --inputs
+time parallel python3 load_tweets_batch.py --db=postgresql://postgres:pass@localhost:13512/ --inputs={} ::: $files
